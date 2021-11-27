be6e6177b5e9a661eded47291a1e5764056f3467
Ali_Rahmani <ali93rahmani@gmail.com>
2021-11-28 01:46:09 +0330

    1400-09-07: image and result CRUD

--- app/models.py
+++ app/models.py
@@ -1,8 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
-class A(models.Model):
+class Log(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    modify_time = models.DateTimeField(auto_now=True)
+
+
+class ClientRequest(Log):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField()
-    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(null=True, blank=True, help_text="what is your image")
+    variable_1 = models.SmallIntegerField()
+    method = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
+    pass
+
+
+class ModelResult(Log):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    client_request = models.OneToOneField(ClientRequest, on_delete=models.CASCADE)
+    result = models.ImageField()
+
+    def __str__(self):
+        return self.client_request.image
+    pass
--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 import views
 
 urlpatterns = [
-    # path('/', views),
+    path('images/get-all', views.get_all__image, name="all-images"),
+    path('images/get', views.get__image, name="get-images"),
+    path('images/add', views.add__image, name="add-images"),
+    path('images/delete', views.delete__image, name="delete-images"),
+    path('images/update', views.update__image, name="update-images"),
+
+    path('result/get-all', views.get_all__result, name="all-results"),
+    path('result/get', views.get__result, name="get-results"),
+    path('result/add', views.add__result, name="add-results"),
+    path('result/delete', views.delete__result, name="delete-results"),
+
 ]
